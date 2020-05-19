4fc88902b014eebc83217e830f19d15ffd282834
fankff <1990432344@qq.com>
2020-05-19 09:55:12 +0800

    增加 ribbon 负载均衡，服务调用 2

---
+++ spring-mycat/src/main/java/com/fankf/SpringMycatApplication.java
@@ -0,0 +1,13 @@
+package com.fankf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMycatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMycatApplication.class, args);
+    }
+
+}
