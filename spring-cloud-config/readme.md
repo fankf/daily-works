32de328a227e30c6fc0efcd23c42bfbb05b9ff82
fankff <1990432344@qq.com>
2020-06-03 09:58:33 +0800

    spring cloud config client

---
+++ spring-cloud-config/readme.md
@@ -0,0 +1,7 @@
+## 请求配置中心地址 
+> [] 可选
+* /{application}/{profiles}/[{labal}]
+* /{application}-{profiles}.yml
+* /{application}-{profiles}.properties
+* /{label}/{application}-{profiles}.yml
+* /{label}/{application}-{profiles}.properties
--- spring-cloud-eureka-service/src/main/java/com/fankf/controller/Test2Controller.java
+++ spring-cloud-eureka-service/src/main/java/com/fankf/controller/Test2Controller.java
@@ -1,5 +1,6 @@
 package com.fankf.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test2Controller {
 
+    @Value("k")
+    private String k;
     @RequestMapping("/sayHello2/{name}")
     public String sayHello(@PathVariable("name") String name) {
         return "hello world! hello2," + name;
+    }
+
+    @RequestMapping("/hi")
+    public String sayhi() {
+        return "hello world! hello2," + k;
     }
 }
