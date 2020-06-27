29da90ed6867d7e085d67dac2cb839ea83da31fa
fankf <1990432344@qq.com>
2020-06-27 10:40:24 +0800

    文件处理0

---
+++ ofd-file/src/main/java/com/dxhy/ofdfile/OfdFileApplication.java
@@ -0,0 +1,13 @@
+package com.dxhy.ofdfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfdFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfdFileApplication.class, args);
+    }
+
+}
---
+++ ofd-file/src/test/java/com/dxhy/ofdfile/OfdFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxhy.ofdfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfdFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
