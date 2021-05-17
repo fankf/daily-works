8519e9c08796a22d0411115c319afd4c40095ecd
fankf <kunfeng.fan@wenge.com>
2021-05-17 19:25:16 +0800

    style(spring cloud alibaba): nacos-config

---
+++ spring-cloud-alibaba/daily-start-nacos-config/src/main/java/com/fankf/config/DailyStartNacosConfigApplication.java
@@ -0,0 +1,19 @@
+package com.fankf.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyStartNacosConfigApplication {
+
+//    @Value("${username}")
+//    private static String username;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyStartNacosConfigApplication.class, args);
+
+//        System.out.println(username);
+    }
+
+}
---
+++ spring-cloud-alibaba/daily-start-nacos-config/src/test/java/com/fankf/config/DailyStartNacosConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fankf.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyStartNacosConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
