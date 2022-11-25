4a9b0d4d79c1ef41a0e2d5c7cdad8c39aaed8e7d
fankunfeng <fankunfeng@hzys.net.cn>
2022-11-25 13:22:36 +0800

    fix(1.0):
    更新依赖版本2

--- spring-cloud-alibaba/daily-start-nacos-config/src/main/java/com/fankf/config/DailyStartNacosConfigApplication.java
+++ spring-cloud-alibaba/daily-start-nacos-config/src/main/java/com/fankf/config/DailyStartNacosConfigApplication.java
@@ -3,17 +3,26 @@ package com.fankf.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DailyStartNacosConfigApplication {
 
-    @Value("${username}")
-    private static String username;
+    @Value("${uaa}")
+    private String username;
 
     public static void main(String[] args) {
         SpringApplication.run(DailyStartNacosConfigApplication.class, args);
 
+
+    }
+
+    @GetMapping("/test")
+    String test(){
         System.out.println(username);
+        return username;
     }
 
 }
